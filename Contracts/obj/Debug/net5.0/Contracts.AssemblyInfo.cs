5412e8abef187c5635a1d291820ae0c84fb8ccf0
Muniro <Munir_Syed@hotmail.com>
2022-03-10 14:59:14 +0000

    adds new files

--- Contracts/obj/Debug/net5.0/Contracts.AssemblyInfo.cs
+++ Contracts/obj/Debug/net5.0/Contracts.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Contracts")]
+[assembly: System.Reflection.AssemblyProductAttribute("BCE-Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
